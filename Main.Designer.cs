d4ff5b99fdf2722b913978d4e80859a80c256cb1
asupstudent <asupstudent@yandex.ru>
2024-01-11 15:30:10 +0300

    Добавьте файлы проекта.

---
+++ Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Snowflake
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 677);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(984, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(226, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество итераций";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.YellowGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(893, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Запуск";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Render);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1024, 768);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1024, 768);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Курсовой проект Валерия Буканова - Снежинка Коха";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Snowflake
+{
+    public partial class Main : Form
+    {
+        static Pen pencil1;
+        static Pen pencil2;
+        static Graphics g;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Render(object sender, EventArgs e)
+        {
+            //Цвет первого карандаша 
+            pencil1 = new Pen(Color.Lime, 1);
+            //Цвет второго карандаша 
+            pencil2 = new Pen(Color.Orange, 1);
+            //Холст
+            g = CreateGraphics();
+            //Заливаем холст черным цветом
+            g.Clear(Color.Black);
+
+            //Координаты исходного треугольника
+            var point1 = new PointF(250, 250);
+            var point2 = new PointF(625, 250);
+            var point3 = new PointF(437.5f, 500);
+
+            //Рисуем исходный треугольник
+            g.DrawLine(pencil1, point1, point2);
+            g.DrawLine(pencil1, point2, point3);
+            g.DrawLine(pencil1, point3, point1);
+
+            //Три кривых Коха на сторонах треугольника
+            RenderKoсh(point1, point2, point3, (int)this.numericUpDown1.Value);
+            RenderKoсh(point2, point3, point1, (int)this.numericUpDown1.Value);
+            RenderKoсh(point3, point1, point2, (int)this.numericUpDown1.Value);
+        }
+
+        static int RenderKoсh(PointF p1, PointF p2, PointF p3, int iteration)
+        {
+            if (iteration > 0)
+            {
+                //Средняя треть отрезка
+                var p4 = new PointF((p2.X + 2 * p1.X) / 3, (p2.Y + 2 * p1.Y) / 3);
+                var p5 = new PointF((2 * p2.X + p1.X) / 3, (p1.Y + 2 * p2.Y) / 3);
+                //Координаты вершины угла
+                var ps = new PointF((p2.X + p1.X) / 2, (p2.Y + p1.Y) / 2);
+                var pn = new PointF((4 * ps.X - p3.X) / 3, (4 * ps.Y - p3.Y) / 3);
+                //Отрисовка
+                g.DrawLine(pencil1, p4, pn);
+                g.DrawLine(pencil1, p5, pn);
+                g.DrawLine(pencil2, p4, p5);
+
+                //Рекурсия
+                RenderKoсh(p4, pn, p5, iteration - 1);
+                RenderKoсh(pn, p5, p4, iteration - 1);
+                RenderKoсh(p1, p4, new PointF((2 * p1.X + p3.X) / 3, (2 * p1.Y + p3.Y) / 3), iteration - 1);
+                RenderKoсh(p5, p2, new PointF((2 * p2.X + p3.X) / 3, (2 * p2.Y + p3.Y) / 3), iteration - 1);
+
+            }
+            return iteration;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
